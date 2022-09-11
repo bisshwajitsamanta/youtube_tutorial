6c117ebd1dc189456d6180d3b16689c7c762972a
Bisshwajit Samant <bisshwajit.samanta@gmail.com>
2022-09-11 23:02:42 +0530

    Grabbing IP Address

---
+++ golang_series/GrabIP/grabIp.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+/*
+	The program should grab the IP address from any API endpoint and displays on the terminal
+*/
+
+func main() {
+	// Find some method to get the http request
+	resp, err := http.Get("https://api.ipify.org?format=json")
+
+	// Handle Error
+	if err != nil {
+		fmt.Printf("Unable to grab the data from the Api Endpoint:%w", err.Error())
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(data))
+}
