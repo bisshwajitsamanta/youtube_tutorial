19624bc42d18bd5acdaaf1ee136d370071875de4
Bisshwajit Samant <bisshwajit.samanta@gmail.com>
2022-09-12 13:06:24 +0530

    TCP Scanner in a function based approach

--- golang_series/ethical_hacking/tcp_scanner/tcpScan.go
+++ golang_series/ethical_hacking/tcp_scanner/tcpScan.go
@@ -6,19 +6,23 @@ import (
 	"strconv"
 )
 
-func main() {
+func ScanPort(port int) {
 	ip := "scanme.nmap.org"
+	address := net.JoinHostPort(ip, strconv.Itoa(port))
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		// Port is closed or filtered
+		return
+	}
+	defer conn.Close()
+	fmt.Printf("[+] Connection Established ... PORT:%d %s\n", port, conn.RemoteAddr().String())
+}
+
+func main() {
 
 	//for i := 1; i < 100; i++ {
 	for i := range [100]int{} {
-		address := net.JoinHostPort(ip, strconv.Itoa(i))
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			// Port is closed or filtered
-			continue
-		}
-		defer conn.Close()
-		fmt.Printf("[+] Connection Established ... PORT:%d %s\n", i, conn.RemoteAddr().String())
+		ScanPort(i)
 	}
 
 }
