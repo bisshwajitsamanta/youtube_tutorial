add6f344db832ee649a5f1b4e323693984e73d19
Bisshwajit Samant <bisshwajit.samanta@gmail.com>
2022-10-26 19:23:20 +0530

    Kubernetes Setup Basic Connection

---
+++ golang_series/kubernetes/BasicConnection/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"log"
+	"path/filepath"
+)
+
+func main() {
+	client, err := GetClient()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("Test:%+v", client)
+}
+
+func GetClient() (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
+}
