3c7df06dc7b83d782cd4378291059ee7d4e30d5c
Bisshwajit Samant <bisshwajit.samanta@gmail.com>
2023-01-18 17:21:09 +0530

    Code ran

--- golang_series/Interview/alternateSliceCharacters.go
+++ golang_series/Interview/alternateSliceCharacters.go
@@ -2,16 +2,6 @@ package main
 
 import "fmt"
 
-/*
-	Alternate Characters Deletion
-		AAAA => returns 3 which needs to be deleted
-		AABAABB => returns 2 A's which needs to be deleted
-
-	Approach
-		I need a map which says if the key found then dont add it, else add it.
-
-*/
-
 func RemoveCharacters(s []string) int {
 	for i := 0; i < len(s)-1; i++ {
 		for j := i + 1; j < len(s); j++ {
