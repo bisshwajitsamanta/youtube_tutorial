0eb21b3e104d73376efe74a3dc5b36919eef9c32
Bisshwajit Samant <bisshwajit.samanta@gmail.com>
2023-01-22 00:02:04 +0530

    Code ran

--- golang_series/Interview/StringManipulation.go
+++ golang_series/Interview/StringManipulation.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//type Resps struct {
+//	Timestamp    string
+//	Count        int
+//	ResponseCode string
+//}
+
+func main() {
+	file, err := os.ReadFile("./stringmanipulationset.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	data := string(file)
+	m := make(map[string][]string)
+	scanners := bufio.NewScanner(strings.NewReader(data))
+
+	for scanners.Scan() {
+		value := strings.SplitAfter(scanners.Text(), "responseCode")
+		timeStamp, respCode := value[0][:8], value[1][:4]
+
+		if val, ok := m[respCode]; ok {
+			// Repeat value
+			// Val is assigned to the value of the map[string][]string
+
+			m[respCode] = append(val, timeStamp)
+
+		} else {
+			// First Timer
+			m[respCode] = []string{timeStamp}
+		}
+	}
+	for k, v := range m {
+		fmt.Println(k, v, len(v))
+	}
+	//fmt.Println(m)
+
+}
--- golang_series/Interview/passwordValidation.go
+++ golang_series/Interview/passwordValidation.go
@@ -1,1 +1,75 @@
 package main
+
+/* Letters should have
+1 special character
+1 number
+1 lower case
+1 upper case
+1 special character
+Total Length of the password should be 6 => Done
+The string contains all those and number is 6
+
+*/
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func Validation(password string) int {
+	var count int
+	var (
+		numeric, lowercase, uppercase, specialCharacters bool
+	)
+
+	//desired := 6
+	// Check whether the length of the password is 6 or not
+	//if len(password) >= desired {
+	numeric = regexp.MustCompile(`\d`).MatchString(password)
+	fmt.Println("Numeric:", numeric)
+	lowercase = regexp.MustCompile(`[a-z]`).MatchString(password)
+	fmt.Println("lowercase:", lowercase)
+	uppercase = regexp.MustCompile(`[A-Z]`).MatchString(password)
+	fmt.Println("uppercase:", uppercase)
+	specialCharacters = strings.ContainsAny(password, "!@#$%^&*()-+")
+	fmt.Println("specialCharacters:", specialCharacters)
+
+	// Count as integer
+	//if len(password) < 6 {
+	//	count = 6 - len(password)
+	//	return count
+	//}
+	if !numeric {
+		count++
+	}
+	if !uppercase {
+		count++
+	}
+	if !lowercase {
+		count++
+	}
+	if !specialCharacters {
+		count++
+	}
+
+	// length =47 =2<6
+	// count = 3
+
+	if len(password)+count < 6 {
+		count = count + 6 - (len(password) + count)
+		// 3 + 6-(2+3)
+	}
+
+	//else {
+	//if len(password) < 6 {
+	//charactersRequired = desired - len(password)
+	//return charactersRequired
+	//}
+	return count
+
+}
+
+func main() {
+	fmt.Println(Validation("47"))
+}
